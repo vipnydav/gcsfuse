cc4dec95acd7f018073d9891924c2d34f1de05d9
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-12-18 11:27:20 +0530

    Update e2e_test.sh

--- tools/cd_scripts/e2e_test.sh
+++ tools/cd_scripts/e2e_test.sh
@@ -207,6 +207,7 @@ else
     sudo dnf -y install dnf-plugins-core
     sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
     sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo usermod -aG docker $USER
 fi
 
 # install go
